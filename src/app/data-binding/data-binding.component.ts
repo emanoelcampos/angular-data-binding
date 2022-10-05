33b0dfd657d27bc5600901560293d1c6a7eb06ce
emanoelcampos <emanoel.cv@gmail.com>
2022-10-05 00:11:57 -0300

    two-way data binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -16,6 +16,13 @@ export class DataBindingComponent implements OnInit {
 
   isMouseOver: boolean = false;
 
+  nome: string = 'emanoel';
+
+  pessoa: any = {
+    nome: 'emanoel',
+    idade: 25
+  }
+
   getValor() {
     return 1;
   }
